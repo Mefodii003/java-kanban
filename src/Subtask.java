85b6f6398a45ebc58cf2fc24a8efd31ceb69393c
Mefodii003 <misha.mih003@gmail.com>
2025-07-23 05:25:10 +0300

    3 коммит 6 спринт. Исправить на что ругается git PR.

--- src/Subtask.java
+++ src/Subtask.java
@@ -1,16 +1,26 @@
-public class Subtask extends Task {
+public class Subtask extends Task
+{
     private int epicId;
 
-    public Subtask(String title, String description, TaskStatus status, int epicId) {
+    public Subtask(String title, String description, TaskStatus status, int epicId)
+    {
         super(title, description, status);
         this.epicId = epicId;
     }
 
-    public int getEpicId() { return epicId; }
-    public void setEpicId(int epicId) { this.epicId = epicId; }
+    public int getEpicId()
+    {
+        return epicId;
+    }
+
+    public void setEpicId(int epicId)
+    {
+        this.epicId = epicId;
+    }
 
     @Override
-    public String toString() {
+    public String toString()
+    {
         return "Subtask{" +
                 "id=" + getId() +
                 ", title='" + getTitle() + '\'' +
