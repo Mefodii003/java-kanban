04acd108350ca76eb6160623e9f6d3a164fe2f6f
Mefodii003 <misha.mih003@gmail.com>
2025-07-23 05:10:56 +0300

    2 коммит 6 спринт. Исправить на что ругается git PR.

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -3,26 +3,41 @@ import java.util.List;
 public interface TaskManager {
     // TASK
     void createTask(Task task);
+
     List<Task> getAllTasks();
+
     void deleteAllTasks();
+
     Task getTask(int id);
+
     void updateTask(Task task);
+
     void deleteTask(int id);
 
     // EPIC
     void createEpic(Epic epic);
+
     List<Epic> getAllEpics();
+
     void deleteAllEpics();
+
     Epic getEpic(int id);
+
     void updateEpic(Epic epic);
+
     void deleteEpic(int id);
 
     // SUBTASK
     void createSubtask(Subtask subtask);
+
     List<Subtask> getAllSubtasks();
+
     void deleteAllSubtasks();
+
     Subtask getSubtask(int id);
+
     void updateSubtask(Subtask subtask);
+
     void deleteSubtask(int id);
 
     // SUBTASKS of specific epic
